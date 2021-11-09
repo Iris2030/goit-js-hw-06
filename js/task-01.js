02b59aa06ddee0d64799f055fd7a0f40f57a1956
Iris2030 <ibazhinova@gmail.com>
2021-11-09 10:59:37 +0200

    task 1 fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,18 @@
- 
-const ulMainEl = document.querySelectorAll('#categories .item')
- 
-console.log(`Number of categories: ${ulMainEl.length}`);
-
-
 
 const titleEl = document.querySelectorAll('.item  h2')
- 
- 
+
+
 const liEl = document.querySelectorAll('.item')
 
-  
 
-const makeMessage = function(param1, param2)   {
-  
-      return `Category: ${param1}
-Elements: ${param2}`
-  
-}
+console.log(`Number of categories: ${liEl.length}`);
+
  
-console.log(makeMessage(titleEl[0].textContent, liEl[0].lastElementChild.children.length));
-console.log(makeMessage(titleEl[1].textContent, liEl[1].lastElementChild.children.length));
-console.log(makeMessage(titleEl[2].textContent, liEl[2].lastElementChild.children.length));
+ 
+
+liEl.forEach(el => {
+      console.log(`Category: ${el.querySelector('h2').textContent}
+      Elements: ${el.querySelectorAll("ul li" ).length}  `); 
+})
+ 
  
