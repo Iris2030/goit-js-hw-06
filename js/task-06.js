5a8e66be039c42769bf939b177d2856a20a7541d
Iris2030 <ibazhinova@gmail.com>
2021-11-09 17:47:47 +0200

    6 fixed again

--- js/task-06.js
+++ js/task-06.js
@@ -7,22 +7,27 @@ inputEl.addEventListener("blur", onInputBlur)
 
 function onInputBlur(event) {
 
-       const hasClassValid = document.querySelector(".valid")
+    //    const hasClassValid = document.querySelector(".valid")
     
-    hasClassValid?.classList.remove("valid")
+    // hasClassValid?.classList.remove("valid")
     
     if ( event.currentTarget.value.length === Number(event.currentTarget.dataset.length))  {
  
        classAdd('valid')
+       classRemove("invalid")
     }
     
-    const hasClassInvalid = document.querySelector(".invalid")
+    // const hasClassInvalid = document.querySelector(".invalid")
     
-    hasClassInvalid?.classList.remove("invalid")
+    // hasClassInvalid?.classList.remove("invalid")
     
-    if(event.currentTarget.value.length !== Number(event.currentTarget.dataset.length)) {
-        classAdd("invalid")
+    // if(event.currentTarget.value.length !== Number(event.currentTarget.dataset.length)) {
+    //     classAdd("invalid")
+    // }
+    else {
+         classAdd('invalid')
     }
+    
   
 }
 
@@ -30,4 +35,6 @@ function classAdd(newclass) {
     inputEl.classList.add(newclass)
 }
 
- 
+ function classRemove(oldclass) {
+    inputEl.classList.remove(oldclass)
+}
